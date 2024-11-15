72523b3b60cf9556e11d2b6c2a006a4a6c6da823
Aurelius Ivan Wijaya <aureliusivanwijaya@gmail.com>
2024-11-15 18:58:00 +0700

    chore: remove unused dependencies and variables

--- client/src/app/game/page.tsx
+++ client/src/app/game/page.tsx
@@ -2,20 +2,19 @@
 
 import React, {Suspense} from 'react'
 import dynamic from 'next/dynamic'
-import {SocketProvider} from "@/contexts/SocketContext";
 import {Chat} from "@/components/chat";
 
 const AppWithoutSSR = dynamic(() => import("@/components/game/App"), {ssr: false});
 
 export default function Home() {
   return (
-        <div className="flex w-full h-full">
-          <Suspense fallback={<div>Loading...</div>}>
-            <AppWithoutSSR/>
-          </Suspense>
-          <div className="fixed top-0 right-0  w-64">
-            <Chat/>
-          </div>
+      <div className="flex w-full h-full">
+        <Suspense fallback={<div>Loading...</div>}>
+          <AppWithoutSSR/>
+        </Suspense>
+        <div className="fixed top-0 right-0  w-64">
+          <Chat/>
         </div>
+      </div>
   )
 }
